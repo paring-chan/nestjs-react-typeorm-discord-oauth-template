b766d6ce4f52920b47864e898558a9759c00d19d
파랑이 <parangee9706@gmail.com>
2021-07-29 14:18:33 +0900

    install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { AuthModule } from './auth/auth.module'
 import * as path from 'path'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
     imports: [
@@ -9,6 +10,7 @@ import * as path from 'path'
             rootPath: path.join(__dirname, '../client/build'),
         }),
         AuthModule,
+        TypeOrmModule.forRoot(),
     ],
 })
 export class AppModule {}
